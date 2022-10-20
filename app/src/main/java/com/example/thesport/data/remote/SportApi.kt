4ec7e7e3534a0bc5caac7a38b4ae052c55b8ab6f
bradley <bradley@scgcanada.com>
2022-10-20 11:06:20 -0400

    Added logic to hide api keys from repo

--- app/src/main/java/com/example/thesport/data/remote/SportApi.kt
+++ app/src/main/java/com/example/thesport/data/remote/SportApi.kt
@@ -1,5 +1,6 @@
 package com.example.thesport.data.remote
 
+import com.example.thesport.BuildConfig
 import com.example.thesport.domain.model.League
 import com.example.thesport.domain.model.Status
 import retrofit2.Retrofit
@@ -9,7 +10,6 @@ import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface SportApi {
-
     @Headers(
         "x-rapidapi-host: $BASE_URL",
         "x-rapidapi-key: $API_KEY"
@@ -27,9 +27,8 @@ interface SportApi {
 
 
     companion object {
-        const val API_KEY = "a3fa8171e98ed0d24d9fad81f9a99dd0"
+        const val API_KEY = BuildConfig.API_KEY
         const val BASE_URL = "https://v1.hockey.api-sports.io"
-        const val API_KEY_B = "45d2c07ca2d5a585d737fe26afc75a89"
     }
 
 
