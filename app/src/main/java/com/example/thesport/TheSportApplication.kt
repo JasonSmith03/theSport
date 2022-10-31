bb3e263bfb67ea4c291bd03ef64d37839a03182c
Bradley Rose <rose.bradley9@gmail.com>
2022-10-31 11:35:12 -0400

    Added Dagger hilt di and files

---
+++ app/src/main/java/com/example/thesport/TheSportApplication.kt
@@ -0,0 +1,7 @@
+package com.example.thesport
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TheSportApplication: Application()
---
+++ app/src/main/java/com/example/thesport/di/AppModule.kt
@@ -0,0 +1,12 @@
+package com.example.thesport.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    // TODO - provide SportApi instance
+}
